9a3350d62265590e662bbbbbd0663a9d84e3e918
Kevin Sweeren <sweerenk@gmail.com>
2018-11-23 09:24:59 +0100

    eerste versie

---
+++ testConferentie/src/main/java/com/testConferentie/testConferentie/TestConferentieApplication.java
@@ -0,0 +1,12 @@
+package com.testConferentie.testConferentie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestConferentieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestConferentieApplication.class, args);
+	}
+}
---
+++ testConferentie/src/test/java/com/testConferentie/testConferentie/TestConferentieApplicationTests.java
@@ -0,0 +1,16 @@
+package com.testConferentie.testConferentie;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestConferentieApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
